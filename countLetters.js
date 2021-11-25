4c2175a5399e3f36b58bb91ea92aa25f304524e3
Julia Gatina <julia.sarandi@gmail.com>
2021-11-25 18:15:11 +0000

    Updates

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = (actual, expected) => {
+  let message = '';
+  if (actual === expected) {
+    message = `😀 😀 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `😞 😞 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(message);
+};
+
+const countLetters = function (stringValue) {
+  const result = {};
+  for (const char of stringValue) {
+    if (' ' !== char) {
+      if (result[char]) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters('lighthouse in the house'));
