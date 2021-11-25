102c2d52149db1a915726ddf7b2f1ae92bc1ddd8
Julia Gatina <julia.sarandi@gmail.com>
2021-11-25 20:44:15 +0000

    Adding files for lotide project

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+// Task: function that can compare two arrays for a perfect match.
+
+const assertEqual = function (actual, expected) {
+  let message = '';
+  if (actual === expected) {
+    message = `😀 😀 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `😞 😞 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(message);
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => true
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
---
+++ letterPositions.js
@@ -0,0 +1,62 @@
+// Function that will return all the indices (zero-based positions)
+// in the string where each character is found.
+
+const assertArraysEqual = function (array1, array2) {
+  let isEquals = true;
+  let message = '';
+
+  // 1. compare lengths, if length is not equal, no need to compare elements
+  if (array1.length !== array2.length) {
+    return (isEquals = false);
+  } else {
+    // 2. compare each element
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return (isEquals = false);
+        break;
+      }
+    }
+  }
+
+  if (isEquals) {
+    message = `😀 😀 Assertion Passed: ${array1} === ${array2}`;
+  } else {
+    message = `😞 😞 Assertion Failed: ${array1} !== ${array2}`;
+  }
+  console.log(message);
+  return true;
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// hello
+const letterPositions = function (sentence) {
+  const sentenceToArrayOfChars = sentence.split('');
+  const results = {};
+
+  for (let i = 0; i < sentenceToArrayOfChars.length; i++) {
+    const char = sentenceToArrayOfChars[i];
+    if (results[char] !== ' ') {
+      if (!results[char]) {
+        results[char] = [i];
+      } else {
+        results[char].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions('hello'));
+
+assertArraysEqual(letterPositions('hello').e, [1]);
