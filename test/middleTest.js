dc7fb62122f720c7c700173aeb259a50adf002e0
Julia Gatina <julia.sarandi@gmail.com>
2021-11-30 03:38:07 +0000

    Updated middleTest and tailTest files to use mocha/chai, added node_modules to .gitignore

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+const middle = require("../middle")
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [5]);
+describe("#middle function", () => {
+  it("Returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+
+  it("Returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4])
+  });
+});
+
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const expected = tail(['Hello', 'Lighthouse', 'Labs']);
-const actual = ['Lighthouse', 'Labs']
-assertArraysEqual(expected, actual);
+describe("#tail function", () => {
+
+  it("Returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs'])
+  });
+
+  it("Returns [3, 4, 5] for [2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([2, 3, 4, 5]), [3, 4, 5])
+  });
+});
